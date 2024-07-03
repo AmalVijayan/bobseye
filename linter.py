e7dd7f9329d4cffdabb475be30d57d797001eaac
Amal <amalvnair95@gmail.com>
2024-07-03 19:28:06 +0530

    [version/0.2] - added comments

--- linter.py
+++ linter.py
@@ -8,32 +8,40 @@ def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
 
 class AILinter:
+
     def __init__(self, model_name):
         self._model = ChatOpenAI(model=model_name)
         self.results = []
-        
 
+    # Creates an LLM chain and invokes with the input snippets
     def lint_code_snippets_with_rag(self, inputs, retriever):
         ai_model_chain = self._create_rag_chain(retriever)
         self.results = self._process_inputs(inputs, ai_model_chain)
 
+    # Creates an LLM chain that can be invoked with an input
     def _create_rag_chain(self, retriever):
         rag_chain = (
-            {"context": retriever | format_docs, "snippet": RunnablePassthrough()}
+            {
+                "context": retriever | format_docs, 
+                "snippet": RunnablePassthrough()
+                }
             | self._create_prompt_template()
             | self._model
             | StrOutputParser()
             )
         return rag_chain
     
+    # Returns a formatted prompt
     def _create_prompt_template(self):
         return ChatPromptTemplate.from_messages(
             [("system", RAG_PROMPT), ("user", "{snippet}"),]
         )
 
+    # calls the LLM for each input
     def _process_inputs(self, inputs, ai_model_chain):
         return [ai_model_chain.invoke(input) for input in inputs]
-
+    
+    #writes the results to an output file
     def write_results_to_file(self, file_name):
         with open(file_name, 'w', encoding='utf-8') as file:
             for index, result in enumerate(self.results):
--- vector.py
+++ vector.py
@@ -10,6 +10,7 @@ class PineconeVectorDB:
         self.db = Pinecone(api_key=api_key)
         self.embedding = embedding
 
+    # creates an index of given name in the PineconeDB
     def create_index(self, index_name):
         self.db.create_index(
             name=index_name,
@@ -21,15 +22,18 @@ class PineconeVectorDB:
             )
         )
 
+    # loads, splits and stores the into the index created above
     def init_pineconedb_with_docs(self, filename, index):
         context_docs = self._split_and_index_source_documet(filename)
         PineconeVectorStore.from_documents(context_docs, self.embedding, index_name=index)
 
+    # Loads and splits the data into chunks
     def _split_and_index_source_documet(self, filename):
         docs = TextLoader(filename).load()
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
         return text_splitter.split_documents(docs)
         
+    # Returns the vectorstore for retrieving
     def get_vectore_store(self, index):
         return PineconeVectorStore(
         index_name=index, embedding=self.embedding)
